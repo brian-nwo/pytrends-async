a5b3311b6dbb7285ea18f8af83029dd9de7867a6
brian-nwo <96438192+brian-nwo@users.noreply.github.com>
2022-01-24 18:38:46 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ import os
 from setuptools import setup, find_packages
 
 dir = os.path.dirname(__file__)
+github_token = os.environ['GITHUB_TOKEN']
 
 with io.open(os.path.join(dir, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
@@ -14,8 +15,8 @@ setup(
     description='Pseudo API for Google Trends with asyncio support.',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/KyleKreutzer/pytrends-async',
-    download_url='https://github.com/KyleKreutzer/pytrends-async/archive/0.3.2.tar.gz',
+    #url='https://github.com/KyleKreutzer/pytrends-async',
+    #download_url='https://github.com/KyleKreutzer/pytrends-async/archive/0.3.2.tar.gz',
     author='Kyle Kreutzer',
     author_email='kyleakreutzer@gmail.com',
     license='Apache 2.0',
@@ -29,5 +30,10 @@ setup(
     ],
     install_requires=['pandas>=0.25', 'lxml', 'httpx==0.9.5', 'tenacity==6.0.0'],
     keywords='google trends api search async asyncio',
-    packages=['pytrendsasync']
+    packages=['pytrendsasync'],
+    install_requires='package',
+    dependency_links = [
+    'git+https://{github_token}@github.com/user/{package}.git/@{version}#egg={package}'
+        .format(github_token=github_token, package='pytrends-async', version=master)
+        ]
 )
